e913109ef6ee8aa745f7422db082fe334a2e96c4
Steven Johnston <wheatthins@hotmail.ca>
2016-09-06 13:16:18 -0400

    Update formsubmit.js

--- formsubmit.js
+++ formsubmit.js
@@ -1,6 +1,5 @@
 
 (function( $ ) {
-
     $.fn.formSubmit = function(url, method, data) {
       var form_html = "";
       form_html += '<form method="'+method+'" action="'+url+'">';
@@ -8,7 +7,6 @@
       form_html += "</form>";
       $(form_html).submit();
     };
-
 }( jQuery ));
 function obj_to_input(obj,my_keys)
 {
@@ -17,17 +15,12 @@ function obj_to_input(obj,my_keys)
   for (var this_key in obj) {
     if (obj.hasOwnProperty(this_key)) {
       var new_my_keys = my_keys.slice();
-
-
       new_my_keys.push(this_key);
-      if(Array.isArray(obj[this_key]))
-      {
+      if(Array.isArray(obj[this_key])){
         html += obj_to_input(obj[this_key], new_my_keys);
-      }
-      else if (typeof obj[this_key] === 'object' && obj[this_key] !== null) {
+      }else if (typeof obj[this_key] === 'object' && obj[this_key] !== null) {
         html += obj_to_input(obj[this_key], new_my_keys);
-      }
-      else {
+      }else {
         var name = '';
         var first = false;
         for (var one_key in new_my_keys) {
@@ -36,8 +29,7 @@ function obj_to_input(obj,my_keys)
             {
               name += new_my_keys[one_key];
               first = true;
-            }
-            else {
+            }else {
               name += "["+new_my_keys[one_key]+"]";
             }
           }
